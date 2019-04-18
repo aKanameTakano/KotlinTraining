872716b8c1a93555591bb2456dfcb59357985290
KanameTakano <kaname.takano@access-company.com>
2019-04-18 11:02:08 +0900

    add func hello

--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -12,10 +12,18 @@ class SampleTest {
         val expected = 1
         val actual = 1
         assertEquals(expected, actual, "[want] $expected [got] $actual")
+
+
+        //hello Funcを呼びだす
+        hello()
     }
 
-    @Test
-    fun testHello() {
-        // TODO: implement to call hello function
+//    @Test
+//    fun testHello() {
+//        // TODO: implement to call hello function
+//    }
+
+    fun hello(){
+        println("HEllo Wortld")
     }
 }
